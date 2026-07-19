27bb7bf5f8b689ec579eda3e53589f6929c985cb
Manudev2 <manudev8924@gmail.com>
2026-07-19 13:07:01 +0530

    Time: 0 ms (100%), Space: 8.5 MB (12.08%) - LeetHub

---
+++ 2469-convert-the-temperature/2469-convert-the-temperature.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    vector<double> convertTemperature(double celsius) {
+       vector<double> ans;
+        double kel=celsius+273.15;
+        double fah=celsius*1.80+32.00;
+        ans.push_back(kel);
+        ans.push_back(fah);
+        return ans;
+    }
+};
